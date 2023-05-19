5f581e9e5ecc06feadea436a9df0f0f2e1dda62f
Minosanh <125282378+Minosanh@users.noreply.github.com>
2023-05-19 10:40:42 +0700

    commit all

---
+++ Baithuchanhlab2/src/Bai1.java
@@ -0,0 +1,46 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+ 
+ public class Bai1{
+     
+ 
+     private static Scanner scanner;
+
+    public static void main(String[]args){
+         Scanner scanner = new Scanner(System.in);
+         DecimalFormat decimalFormat = new DecimalFormat("#.##");
+
+            System.out.println("Nhap vao so thu nhat:");
+            int firstNumber = scanner.nextInt();
+            System.out.println("Nhap vao so thu hai:");
+            int secondNumber = scanner.nextInt();
+
+    //1.Tinh tong, hieu,tich, thuong, chia lay du cua 2 so do.
+     
+     int tong= firstNumber + secondNumber;
+     System.out.println(firstNumber + "+" + secondNumber + "=" + tong);
+ 
+     int hieu= firstNumber - secondNumber;
+     System.out.println(firstNumber + "-" + secondNumber + "=" + hieu);
+
+     int tich= firstNumber * secondNumber;
+     System.out.println(firstNumber + "*" + secondNumber + "=" + tich);
+
+     float thuong= (float) firstNumber / (float) secondNumber;
+     System.out.println(firstNumber + "/" + secondNumber + "=" + thuong);
+
+     int phanDu= firstNumber % secondNumber;
+     System.out.println(firstNumber + "%" + secondNumber + "=" + phanDu);
+
+    // 2. Su dung các toan tu so sanh da hoc de so sanh 2 so do. 
+            
+     System.out.println("Ket qua so sanh bang cua 2 so " + firstNumber + " va " + secondNumber + " la " + (firstNumber == secondNumber));
+     System.out.println("Ket qua so sanh khong bang cua 2 so " + firstNumber + " va " + secondNumber + " la " + (firstNumber != secondNumber));
+     System.out.println("Ket qua so sanh lon hon cua 2 so " + firstNumber + " va " + secondNumber + " la " + (firstNumber > secondNumber));
+     System.out.println("Ket qua so sanh lon hon hoac bang cua 2 so " + firstNumber + " va " + secondNumber + " la " + (firstNumber >= secondNumber));
+     System.out.println("Ket qua so sanh nho hon cua 2 so " + firstNumber + " va " + secondNumber + " la " + (firstNumber < secondNumber));
+     System.out.println("Ket qua so sanh nho hon hoac bang cua 2 so " + firstNumber + " va " + secondNumber + " la " + (firstNumber <= secondNumber));
+        }
+
+     
+ }
---
+++ Baithuchanhlab2/src/Bai2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ 
+ public class Bai2{
+    public static void main(String[]args){
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println( "Nhap so nguyen:" ); 
+        int number = scanner.nextInt();
+        // so chan chia het cho 2
+    if (number % 2 == 0){
+        System.out.println(number + " la so chan");
+        // nguoc lai la so le
+    } else{
+        System.out.println(number + " la so le");  
+    }
+
+    scanner.close();
+    }
+ }    
